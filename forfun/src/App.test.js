65245123f01dad28d67533660b9ac8c9c2ab5206
roman-jordan <rjordan1352@gmail.com>
2019-09-07 12:41:59 -0700

    Added Test

--- forfun/src/App.test.js
+++ forfun/src/App.test.js
@@ -4,12 +4,12 @@ import App from './App';
 import {render} from '@testing-library/react'
 import Players from './components/Players'
 
-// afterEach(rtl.cleanup);
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+afterEach(rtl.cleanup);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
 test("Players Exist", async () =>{
   const {findByText} = await render(<Players />)
